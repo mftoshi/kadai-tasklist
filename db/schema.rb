60fac6b5631a8f80974d209f74d5988fd42ec45f
m.toshi <mf.toshiki@gmail.com>
2020-08-08 23:45:53 +0000

    0809 fix

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_060057) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.text "status"
   end
 
 end
