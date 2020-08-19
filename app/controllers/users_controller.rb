ddddb618fa574fcb366ae9dab8301d48a35c27d7
m.toshi <mf.toshiki@gmail.com>
2020-08-19 06:04:50 +0000

    20200819 add

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+    #kaminari .page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
